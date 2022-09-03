4213cab66bfcd5c394ef05c5619c81b82d958106
schktjm <schktjm@gmail.com>
2022-09-03 21:59:36 +0900

    :wrench: emotion のインストール

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import styled from '@emotion/styled'
+
+
+const Title = styled.h1`
+  color: pink;
+`
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +19,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Title>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </Title>
 
         <p className={styles.description}>
           Get started by editing{' '}
